1e04fd6b657bcc51e074bf4289b92df3d7078ba9
Valentine Pylypchuk <strheadshot1997@gmail.com>
2023-03-23 17:46:52 +0200

    done task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
@@ -12,8 +13,62 @@ searchInput.addEventListener('input', debounce(onSearchCountry, 1500));
 
 function onSearchCountry() {
     const name = searchInput.value.trim();
-    fetchCountries(name);
+    if (name === '') {
+        return;
+    }
+    fetchCountries(name).then(countries => {
+        if(countries.length > 10) {
+                Notify.info("Too many matches found. Please enter a more specific name.");
+        };
+            clearCountryLists();
+            checkInput(countries);
+        });
        
 }
 
+function checkInput(countries) {
+   
+    if (countries.length === 1) {
+        countryList.insertAdjacentHTML("beforeend", renderCountriesMarkup(countries));
 
+        const countryTitle = countryList.querySelector(".country-list__title");
+        countryTitle.style.fontSize = '45px';
+        countryTitle.style.fontWeight = '800';
+
+        countryInfo.insertAdjacentHTML("beforeend", renderCountriesInfo(countries));
+
+    } else if (countries.length >= 2 && countries.length <= 10) {
+        countryList.insertAdjacentHTML("beforeend", renderCountriesMarkup(countries));
+      
+    }
+
+};
+
+function renderCountriesMarkup(countries) {
+
+    return countries.map(({ flags, name }) => {
+        return `<li class="country-list__item">
+        <img class="country-list__icon" src="${flags.svg}" width=35px height=35px/>
+        <p class="country-list__title">${name.official}</p>
+        </li>`
+    }).join('');
+
+};
+
+
+function renderCountriesInfo(countries) {
+    return countries.map(({ capital, population, languages }) =>
+        `<ul class="country-info__list">
+        <li class="country-info__item"><span class="country-info__description">Capital:</span> ${capital}</li>
+        <li class="country-info__item"><span class="country-info__description">Population:</span> ${population}</li>
+        <li class="country-info__item"><span class="country-info__description">Languages:</span> ${Object.values(languages)}</li>
+        </ul>`
+    ).join('');
+
+};
+
+
+function clearCountryLists() {
+    countryList.innerHTML = '';
+    countryInfo.innerHTML = '';
+};
